d7fb243a67b47e3442b31b52c6ce18e045228e9f
Vo Va <vavan2001228@gmail.com>
2023-06-27 18:33:42 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Currency_Converter
+{
+    public partial class Form1 : Form
+    {
+        private List<string> currencies1 = new List<string>
+        {
+            "USD", "EUR", "GBP", "JPY", "AUD", "CAD" // и другие валюты...
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.AddRange(currencies1.ToArray());
+            comboBox2.Items.AddRange(currencies1.ToArray());
+            
+            textBox1.KeyPress += textBox1_KeyPress;
+            textBox2.KeyPress += textBox2_KeyPress;
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Добавление начального элемента в comboBox1
+            comboBox1.SelectedIndex = 0;
+
+            // Добавление начального элемента в comboBox2
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            CheckingForANumber(e);
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            CheckingForANumber(e);
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == comboBox2.SelectedItem)
+            {
+                int selectedIndex = comboBox2.SelectedIndex;
+                selectedIndex = (selectedIndex + 1) % comboBox2.Items.Count; // Переход к следующему элементу в круговом порядке
+                comboBox2.SelectedIndex = selectedIndex;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == comboBox2.SelectedItem)
+            {
+                int selectedIndex = comboBox1.SelectedIndex;
+                selectedIndex = (selectedIndex + 1) % comboBox1.Items.Count; // Переход к следующему элементу в круговом порядке
+                comboBox1.SelectedIndex = selectedIndex;
+            }
+        }
+
+        private void CheckingForANumber(KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Отменить ввод символа, если это не цифра или управляющий символ
+            }
+        }
+    }
+}
